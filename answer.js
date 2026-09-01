363c47f71b6a05986e7cf8a3ca03bb1c56c20c6d
Tanbiru <tanvir12tt@gmail.com>
2026-09-02 05:12:06 +0600

    Finish

--- answer.js
+++ answer.js
@@ -52,4 +52,53 @@ return "Available";
 
 }
 
+// answer 4
 
+function getCngFare(distance , isNight = false ,waitingMinutes = 0) {
+    let fare = 50;
+
+    if (distance > 2){
+        fare += (distance - 2) * 15;
+        fare += waitingMinutes * 2;
+
+    if(isNight ){
+        fare *= 1.20;
+    }
+    }else if (isNight){
+        fare += waitingMinutes * 2;
+        fare *=1.20;
+    }else{
+        fare += waitingMinutes * 2;
+    }
+      return fare;
+
+}
+
+
+// Answer 5
+
+const getChaseVerdict = (target, scored, ballsLeft) => {
+    const runsNeeded = target - scored;
+
+    if (runsNeeded <= 0) {
+        return "Won";
+    }
+
+    if (ballsLeft <= 0) {
+        return "Lost";
+    }
+
+    const requiredRate = (runsNeeded / ballsLeft) * 6;
+
+    let verdict;
+
+    if (requiredRate <= 6) {
+        verdict = "Comfortable";
+    } else if (requiredRate <= 12) {
+        verdict = "Tough";
+    } else {
+        verdict = "Almost Impossible";
+    }
+
+    return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+};
