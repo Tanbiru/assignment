90d2d50c0dae119e362db46da23f637f58045d9e
Tanbiru <tanvir12tt@gmail.com>
2026-09-02 04:51:12 +0600

    update

--- answer.js
+++ answer.js
@@ -1,3 +1,5 @@
+// Answer 1
+
 function describeValue(value) {
    let type = typeof value;
    let result;
@@ -9,3 +11,45 @@ function describeValue(value) {
    }
    return `${type} | ${result}`;
 }
+
+// Answer 2
+
+function getDayType(day) {
+
+     day = day.toLowerCase();
+
+
+    let weekend = ["friday", "saturday"];
+    let workingDays = ["sunday","monday", "tuesday", "wednesday", "thursday"];
+
+    switch(true){
+        case weekend.includes(day):
+            return "weekend";
+            
+        case workingDays.includes(day):
+            return "Working Day";
+            
+        default:
+            return "invalid day";  
+    }
+}
+
+
+// Answer 3
+
+function validateUsername(username) {
+    if(username.length < 4) {
+        return "Too short";
+        
+}else if (username.includes (" ")){
+     return "No space allowed"; 
+
+}else if (username.toLowerCase().includes("admin")){
+    return "Reserved word";
+}
+return "Available";
+
+
+}
+
+
