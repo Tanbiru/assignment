7959d2f1ab8e6a514faed4a47c26bdcdea0560ba
Tanbiru <tanvir12tt@gmail.com>
2026-09-02 03:21:47 +0600

    first commit

---
+++ answer.js
@@ -0,0 +1,11 @@
+function describeValue(value) {
+   let type = typeof value;
+   let result;
+
+   if(value){
+    result = "truthy";
+   }else{
+    result = "falsy";
+   }
+   return `${type} | ${result}`;
+}
